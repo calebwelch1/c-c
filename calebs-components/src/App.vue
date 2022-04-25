6fcce22253d6d8bdc5f599364b965e699f344af3
Bennington <caleb.welch11@gmail.com>
2022-04-24 22:28:11 -0400

    collapsing navbar, icons

--- calebs-components/src/App.vue
+++ calebs-components/src/App.vue
@@ -3,6 +3,8 @@ import { nextTick } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import useBreakpoints from '@/util/breakPoints.js'
 import navIcon from '@/assets/icons/list_white.svg'
+import chevronDown from '@/assets/icons/chevron_down_white.svg'
+import chevronRight from '@/assets/icons/chevron_right_white.svg'
 
 export default {
   name: 'App',
@@ -13,6 +15,9 @@ export default {
   methods: {
     routeComponentView(){
       this.$router.push('components');
+    },
+    toggleShowComponents(){
+      this.showComponents = !this.showComponents;
     },
     routeAboutView(){
       this.$router.push('about');
@@ -37,6 +42,9 @@ export default {
   data() {
     return {
       navIcon,
+      chevronDown,
+      chevronRight,
+      showComponents: false,
     }
   },
 }
@@ -45,7 +53,18 @@ export default {
 <template>
 <div class="sidenav">
   <img class="nav-icon" @click="showNavButtons" style='color: #fff; height: 5rem; width: 3rem; margin-left: auto; margin-right: 2rem;' :src="navIcon"/>
-  <button class="nav-button" @click="routeComponentView">Components</button>
+  <button class="nav-button" @click="toggleShowComponents">
+    Components
+    <img v-if="!showComponents" :src="chevronRight" style="width: 3rem; height: 1.5rem;"/>
+    <img v-if="showComponents" :src="chevronDown" style="width: 3rem; height: 1.5rem;"/>
+  </button>
+    <transition name="rise">
+    <div v-if="showComponents" class="brand-background" style="width: 100%;">
+      <button class="nav-button-secondary">
+      Buttons
+      </button>
+    </div>
+    </transition>
   <button class="nav-button" @click="routeAboutView">About</button>
   <button class="nav-button" @click="routeContactView">Contact</button>
 </div>
@@ -89,6 +108,11 @@ body{
 .display-none {
   display: none;
 }
+
+.brand-background {
+  background: radial-gradient(circle, rgba(33,36,143,1) 0%, rgba(49,140,115,1) 100%);
+}
+
 // nav
 .sidenav {
   height: 100%;
@@ -104,6 +128,18 @@ body{
 
 .nav-button {
   font-size: 30px;
+  background: transparent;
+  border: none;
+  color: #fff;
+  padding-top: 1vh;
+  padding-bottom: 0.5vh;
+  margin-left: 2vw;
+  display: block;
+  font-weight: 500;
+}
+
+.nav-button-secondary {
+  font-size: 25px;
   background: transparent;
   border: none;
   color: #fff;
@@ -131,5 +167,26 @@ body{
     display: block;
   }
 }
+// transitions
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
 
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.rise-enter-active {
+  transition: all .5s ease;
+}
+.rise-leave-active {
+  transition: all .5s ease;
+}
+.rise-enter-from, .rise-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateY(-10px);
+  opacity: 0;
+}
 </style>
