081cca1e04c5e8741c902678f8abce570f2dd268
Bennington <caleb.welch11@gmail.com>
2022-05-14 22:36:03 -0400

    components view buttons

--- src/views/ComponentViews/Buttons/index.vue
+++ src/views/ComponentViews/Buttons/index.vue
@@ -6,7 +6,17 @@ import BlogHeader from '@/components/BlogHeader.vue'
   <div class="container hide-scroll">
   <BlogHeader title="Buttons" description="Buttons for all of your clicking needs"/>
   <div class="content">
-  Buttons
+    <div class="flex-row" style="margin-top: 3rem; margin-bottom: 3rem;">
+      <button class="orange-box-button">
+        Button
+      </button>
+      <button class="orange-box-button pill" style="margin-left: 20px;">
+        Button
+      </button>
+      <button class="orange-box-button rounded-edge" style="margin-left: 20px;">
+        Button
+      </button>
+    </div>
   </div>
   </div>
 </template>
@@ -17,5 +27,35 @@ import BlogHeader from '@/components/BlogHeader.vue'
   margin-bottom: 5vh;
   padding-right: auto;
   overflow: hidden;
+}
+
+.orange-box-button {
+  border: 0 solid;
+  color: white;
+  background-color:#DA4232;
+  width: 5.5rem;
+  height: 2.2rem;
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: 3px;
+  box-shadow: 0px 5px 20px 1px #D0D0D0;
+}
+
+.orange-box-button:hover {
+  cursor: pointer;
+  box-shadow: 2px 2px 20px 1px #A9A9A9;
+  background-color:#EA4C3B;
+}
+
+.orange-box-button:focus {
+  background-color: #C43425;
+}
+
+.pill {
+  border-radius: 99px;
+}
+
+.rounded-edge {
+  border-radius: 8px;
 }
 </style>
--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,9 +1,73 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script>
+export default {
+  name: 'ComponentsView',
+  data() {
+    return{
+      components:[],
+    };
+  },
+  methods: {
+    routeComponentView(){
+      this.$router.push('components');
+    },
+  },
+}
 </script>
 
 <template>
-  <main>
-    Components View!
-  </main>
+<div class="main">
+<h1 class="text-white title">
+Components
+</h1>
+<router-link to="/buttons" class="text-white support-text">
+&#149;   Buttons
+</router-link>
+<router-link to="/code-box" class="text-white support-text">
+&#149;   Code-Box
+</router-link>
+</div>
 </template>
+
+<style lang="scss">
+
+.title {
+  font-size: 70px;
+  font-weight: 400;
+  margin-top: 25vh;
+  margin-bottom: 2vh;
+  text-align: center;
+}
+.support-text {
+  font-size: 25px;
+  font-weight: 200;
+  margin-top: 5vh;
+  margin-bottom: 2vh;
+  text-align: center;
+  margin-left: 4rem;
+  margin-right: 4rem;
+}
+
+.start-button {
+  font-size: 25px;
+  font-weight: 200;
+  margin-top: 2vh;
+  margin-bottom: 2vh;
+  margin-left: auto;
+  margin-right: auto;
+  width: 10rem;
+}
+
+.main{
+display: grid;
+grid-template-columns: repeat(12, minmax(0, 1fr));
+height: 100vh;
+width:100vw;
+margin:0;
+justify-items: center;
+background: rgb(33,36,143);
+background: radial-gradient(circle, rgba(33,36,143,1) 0%, rgba(49,140,115,1) 100%);
+display: flex;
+flex-direction: column;
+}
+
+</style>
